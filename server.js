496b7d0c48fd56c2ae2503c9446e86e6862f76ea
Annaruth McBride <armcbride17@gmail.com>
2020-06-03 10:33:36 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+
+// app.post("/add", ({ user }, res) => {
+// db.User.create(user)
+//   .then(dbUser => {
+//     console.log(dbUser);
+//     res.json(dbUser);
+//   })
+//   .catch(({ message }) => {
+//     console.log(message);
+//     res.json(err);
+//   });
+// });
+
+
+app.get("/notes", (req, res) => {
+  db.Note.find({})
+    .then(dbNote => {
+      res.json(dbNote);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.post("/submit", ({ user, body }, res) => {
+    db.User.create(user)
+     db.Note.create(body)
+    .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
+    .then(dbUser => {
+      res.json(dbUser);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.get("/user", (req, res) => {
+    db.User.find({})
+      .then(dbUser => {
+        res.json(dbUser);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+app.get("/populateduser", (req, res) => {
+  db.User.find({})
+    .populate("notes")
+    .then(dbUser => {
+      res.json(dbUser);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port http://localhost:${PORT}`);
+});
